ca8bdc78cca36d8a74e69f5df3bc0316fa6c0a2b
quesera2 <glayash2484@gmail.com>
2018-12-24 22:00:26 +0900

    通信処理を Coroutines で置き換え

--- app/src/main/java/sera/sera/que/kotlin201x/screen/search/SearchViewModel.kt
+++ app/src/main/java/sera/sera/que/kotlin201x/screen/search/SearchViewModel.kt
@@ -1,6 +1,5 @@
 package sera.sera.que.kotlin201x.screen.search
 
-import android.os.Handler
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.LiveData
@@ -8,37 +7,41 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.navigation.Navigation.findNavController
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import ru.gildor.coroutines.retrofit.await
 import sera.sera.que.kotlin201x.R
 import sera.sera.que.kotlin201x.api.WikipediaSearchService
-import sera.sera.que.kotlin201x.model.WikipediaPageResponse
 
 class SearchViewModel(
     private val searchService: WikipediaSearchService
-) : ViewModel() {
+) : ViewModel(), CoroutineScope {
 
     val searchQuery = MutableLiveData<String>()
 
     val canExecute: LiveData<Boolean> = Transformations.map(searchQuery) { !it.isNullOrEmpty() }
 
+    private val job = Job()
+    override val coroutineContext = Dispatchers.Main + job
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+
     fun onSearchClick(view: View) {
-        val query = searchQuery.value ?: return
-        val results = searchService.search(query)
-        val navController = findNavController(view)
-        val handler = Handler()
+        launch {
+            try {
+                val query = searchQuery.value ?: return@launch
+                val results = searchService.search(query).await()
+                Log.d("SearchViewModel", results.query.search.toString())
+                findNavController(view).navigate(R.id.action_searchFragment_to_detailFragment)
 
-        results.enqueue(object : Callback<WikipediaPageResponse> {
-            override fun onResponse(call: Call<WikipediaPageResponse>, response: Response<WikipediaPageResponse>) {
-                val result = response.body() ?: return
-                Log.d("TAG", result.toString())
-                handler.post { navController.navigate(R.id.action_searchFragment_to_detailFragment) }
+            } catch (t: Throwable) {
+                Log.e("SearchViewModel", "failure", t)
             }
-
-            override fun onFailure(call: Call<WikipediaPageResponse>, t: Throwable) {
-                Log.e("TAG", "failure", t)
-            }
-        })
+        }
     }
 }
