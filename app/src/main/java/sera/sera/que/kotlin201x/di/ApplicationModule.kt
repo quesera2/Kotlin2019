747812ebb77ac655d483e34bef92f0902e176e01
quesera2 <glayash2484@gmail.com>
2019-02-06 15:58:35 +0900

    詳細画面の WebView 表示処理が動作するように修正（VM の登録など）

--- app/src/main/java/sera/sera/que/kotlin201x/di/ApplicationModule.kt
+++ app/src/main/java/sera/sera/que/kotlin201x/di/ApplicationModule.kt
@@ -5,11 +5,13 @@ import org.koin.dsl.module.module
 import sera.sera.que.kotlin201x.api.WikipediaSearchService
 import sera.sera.que.kotlin201x.api.WikipediaSearchServiceImpl
 import sera.sera.que.kotlin201x.screen.MainViewModel
+import sera.sera.que.kotlin201x.screen.detail.DetailViewModel
 import sera.sera.que.kotlin201x.screen.search.SearchViewModel
 
 val applicationModule = module {
     viewModel<MainViewModel>()
     viewModel<SearchViewModel>()
+    viewModel<DetailViewModel>()
 
     single<WikipediaSearchService> { WikipediaSearchServiceImpl }
 }
--- app/src/main/java/sera/sera/que/kotlin201x/screen/detail/DetailFragment.kt
+++ app/src/main/java/sera/sera/que/kotlin201x/screen/detail/DetailFragment.kt
@@ -5,24 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
-import sera.sera.que.kotlin201x.R
+import org.koin.android.viewmodel.ext.android.viewModel
+import sera.sera.que.kotlin201x.databinding.DetailFragmentBinding
+import sera.sera.que.kotlin201x.screen.detail.DetailFragmentArgs.fromBundle
 
 class DetailFragment : Fragment() {
 
-    private lateinit var viewModel: DetailViewModel
+    private val viewModel: DetailViewModel by viewModel()
+
+    private lateinit var binding: DetailFragmentBinding
+
+    private val page by lazy { fromBundle(requireNotNull(arguments)).page }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.detail_fragment, container, false)
+        binding = DetailFragmentBinding.inflate(inflater, container, false)
+        binding.setLifecycleOwner(this)
+        binding.viewModel = viewModel
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.webView.loadUrl("https://ja.wikipedia.org/w/index.php?curid=${page.pageid}")
+    }
 }
